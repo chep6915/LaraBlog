1926d778c17313bf3118ed17f37125957b34dd6e
nora <noraliao125@gmail.com>
2021-09-12 18:18:00 +0800

    2021.09.12 add google login

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -6,6 +6,11 @@ use App\Bases\BaseController;
 use App\Concretes\IndexConcrete;
 use App\Exceptions\JsException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Laravel\Socialite\Facades\Socialite;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use function PHPUnit\Framework\exactly;
 
 class IndexController extends BaseController
 {
@@ -21,10 +26,47 @@ class IndexController extends BaseController
         parent::__construct($request);
     }
 
-    public function login()
+    public function login(): \Illuminate\Http\JsonResponse
     {
-        return response()->json(['data'=> $this->indexConcrete->login($this->request)]);
+        return response()->json(['data' => $this->indexConcrete->login($this->request)]);
+    }
+
+    public function redirectToProvider(): RedirectResponse
+    {
+        return Socialite::driver('google')->stateless()->redirect();
 
     }
 
+    public function handleProviderCallback()
+    {
+        try {
+            $user = Socialite::driver('google')->stateless()->user();
+            print_r($user);
+            exit();
+//            [token] => ya29.a0ARrdaM8_40cuNCsFQsLIdELKqThCyVNVdIHBo6sCjqViimwnV3jmvNr_fgtyxDKnZleTm4mkTIvYotuZGev3hUsC0G90djKmjYgrHPgeR0yc79owYJK3NQSfwTkaE9OwstdTp_PqBEnqjFjpSh0QXjuBcn2b
+//            [refreshToken] =>
+//            [expiresIn] => 3599
+//            [id] => 106195008024509355308
+//            [nickname] =>
+//            [name] => 謝佳良
+//            [email] => chep6915@gmail.com
+//            [avatar] => https://lh3.googleusercontent.com/a/AATXAJzR5ujUqAAOrDsrQ8zWOQ691dhkQbh2Wyk6z8lD=s96-c
+//            [user] => Array (
+//                [sub] => 106195008024509355308
+//                [name] => 謝佳良
+//                [given_name] => 佳良
+//                [family_name] => 謝
+//                [picture] => https://lh3.googleusercontent.com/a/AATXAJzR5ujUqAAOrDsrQ8zWOQ691dhkQbh2Wyk6z8lD=s96-c
+//                [email] => chep6915@gmail.com
+//                [email_verified] => 1
+//                [locale] => zh-TW
+//                [id] => 106195008024509355308
+//                [verified_email] => 1
+//                [link] =>
+//            )
+//                [avatar_original] => https://lh3.googleusercontent.com/a/AATXAJzR5ujUqAAOrDsrQ8zWOQ691dhkQbh2Wyk6z8lD=s96-c
+//
+        } catch (\Exception $e) {
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,10 @@ use Illuminate\Support\Facades\Route;
 //Route::resource('photo', 'PhotoController');
 Route::prefix("index")->group(function () {
     Route::post('login', [IndexController::class, 'login']);     //登入
-    Route::post('logidn', [IndexController::class, 'login']);     //登入
+//    Route::get('/googleLogin', [IndexController::class, 'googleLogin']);
+//    Route::get('/logout', [IndexController::class, 'logout']);
+    Route::get('/auth', [IndexController::class, 'redirectToProvider']);
+    Route::get('/oauth_callback', [IndexController::class, 'handleProviderCallback']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
