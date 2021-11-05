f7412820c7d9983314602f36b16b1e2cc1543971
Rex <chep6915@gmail.com>
2021-11-05 12:00:32 +0800

    1.audit adminUser

--- database/migrations/2021_09_01_092103_create_admin_users_table.php
+++ database/migrations/2021_09_01_092103_create_admin_users_table.php
@@ -15,14 +15,22 @@ class CreateAdminUsersTable extends Migration
     {
         Schema::create('admin_users', function (Blueprint $table) {
             $table->bigIncrements('id')->comment('系統用户ID');
+            $table->string('avatar_original')->comment('系統用戶原始頭像')->nullable();
+            $table->string('avatar')->comment('系統用戶頭像')->nullable();
+            $table->string('picture')->comment('系統用戶照片')->nullable();
             $table->string('account')->comment('系統用戶帳號');
             $table->string('name')->comment('系統用戶名稱');
+            $table->string('given_name')->comment('名字');
+            $table->string('family_name')->comment('姓氏');
+            $table->string('nick_name')->comment('系統用戶暱稱');
             $table->integer('admin_user_group_id')->comment('系統用戶群組ID');
             $table->string('email', 150)->comment('信箱');
             $table->timestamp('email_verified_at')->comment('信箱認證時間')->nullable();
             $table->string('password')->comment('系統用戶密碼');
             $table->string('admin_user_last_ip')->comment('系統用戶最後登入IP')->nullable();
             $table->integer('last_update_admin_user_id')->comment('最後編輯系統用戶ID');
+            $table->string('admin_user_locale')->comment('系統用戶地區');
+            $table->integer('Google_id')->comment('GoogleId')->nullable();
             $table->integer('is_frozen')->comment('是否被凍結(0:未被凍結,1:被凍結)')->default(0);
             $table->timestamps();
 
--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -18,8 +18,12 @@ class AdminUserSeeder extends Seeder
             [
                 "account" => env("TEST_ACCOUNT"),
                 "email" => env("TEST_EMAIL"),
-                "name" => env("TEST_NAME"),
+                "name" => env("TEST_GIVEN_NAME").' '.env("TEST_FAMILY_NAME"),
+                "given_name" => env("TEST_GIVEN_NAME"),
+                "family_name" => env("TEST_FAMILY_NAME"),
+                "nick_name" => env("TEST_NICK_NAME"),
                 'admin_user_group_id' => 1,
+                'admin_user_locale' => 'zh-TW',
                 'is_frozen' => 0,
             ]
         )->create();
