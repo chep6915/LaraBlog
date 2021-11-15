416011bce5a96361393c02044ddb67876d1e34f9
Red <>
2021-11-15 15:42:35 +0800

    init_login

--- frontend-project/src/main.js
+++ frontend-project/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
@@ -8,7 +10,8 @@ import '@/styles/index.scss'
 import store from './store'
 // import './permission' // permission control
 
-const app = createApp(App)
+const app = Vue.createApp(App)
+app.use(VueAxios, axios)
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
--- frontend-project/src/views/Login.vue
+++ frontend-project/src/views/Login.vue
@@ -1,25 +1,45 @@
 <template>
-    <div class="about" >
-        <el-container  justify="center">
-            <el-header class="header" >
+    <div class="about">
+        <el-container justify="center">
+            <el-header class="header">
                 <!-- <el-button type="warning">我是header</el-button> -->
             </el-header>
-            <el-container class="content" >
-                <el-main class="main" style="display: flex;justify-content: center;">
-                    <el-form  style="width:25vw">
-                        <el-form-item label="帳號">
-                            <el-input  v-model="form.name"  placeholder="請輸入帳號" clearable />
-                        </el-form-item>
-                        <el-form-item label="密碼">
-                            <el-input v-model="form.pwd"  placeholder="請輸入密碼" clearable show-password/>
-                        </el-form-item>
-                        
-                        <el-form-item>
-                            <el-button type="primary" @click="onSubmit"
-                                >登入</el-button
-                            >
-                        </el-form-item>
-                    </el-form>
+            <el-container class="content">
+                <el-main
+                    class="main"
+                    style="display: flex; justify-content: center"
+                >
+                    <el-card shadow="hover" :body-style="{ padding: '20px' }">
+                        <div>
+                            <span>登入LaraBlog</span>
+                        </div>
+                        <el-form label-position="top" style="width: 20vw">
+                            <el-form-item label="帳號">
+                                <el-input
+                                    v-model="form.acc"
+                                    placeholder="請輸入帳號"
+                                    clearable
+                                />
+                            </el-form-item>
+                            <el-form-item label="密碼">
+                                <el-input
+                                    v-model="form.pwd"
+                                    placeholder="請輸入密碼"
+                                    clearable
+                                    show-password
+                                />
+                            </el-form-item>
+
+                            <el-form-item>
+                                <el-button
+                                    style="width: 20vw"
+                                    type="primary"
+                                    @click="onSubmit"
+                                    >登入</el-button
+                                >
+                            </el-form-item>
+                        </el-form>
+                    </el-card>
                 </el-main>
             </el-container>
         </el-container>
@@ -27,37 +47,33 @@
 </template>
 
 <script>
-
+const api = {
+    login: "http://loveenjoylife.com/api/index/login",
+};
+import $ from "jquery";
 import { mapGetters } from "vuex";
-import { defineComponent, ref ,reactive} from "vue";
+import { defineComponent, ref, reactive } from "vue";
 // import { Calendar, Search } from '@element-plus/icons'
 export default defineComponent({
     name: "login",
     components: {},
     setup() {
-
         const form = reactive({
-            account:null,
-            pwd:null
+            acc: null,
+            pwd: null,
         });
-        // const onSubmit = () => {
-        //     axios({
-        //         method:'post',
-        //         url: "http://loveenjoylife.com/api/index/login",
-        //         data: {
-        //             id:requestUrl
-        //         }
-        //     })
-        // }
-        
-        
+        const onSubmit = async () => {
+            let records = await $.post(api.login,form);
+            console.log(`records`, records);
+        };
+
         return {
             radio1: ref("1"),
             radio2: ref("1"),
             radio3: ref("1"),
             radio4: ref("1"),
             form,
-            
+            onSubmit,
         }; // 这里返回的任何内容都可以用于组件的其余部分
     },
     computed: {
